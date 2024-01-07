6a206356ffdf3fb297373d8cf9d95adfda4c48ff
Adesh <adeshkadambi@gmail.com>
2024-01-07 00:42:44 -0500

    cli working

--- flashy.py
+++ flashy.py
@@ -1,14 +1,25 @@
+import typer
+import time
+
+from typing import Optional
+from rich import print
+
 from sqlmodel import Session, SQLModel, create_engine, select
+from models import Flashcard, Tag, FlashcardTagLink
 
-from models import Flashcard, Tag
+# Create the Typer object to define the CLI
+app = typer.Typer()
 
+# Create the database engine
 engine = create_engine("sqlite:///flashy.db")
 
 
 def create_db_and_tables():
+    """Create the database and tables."""
     SQLModel.metadata.create_all(engine)
 
 
+@app.command()
 def add_flashcard(question, answer, tags=None):
     """Add a flashcard to the database."""
     if tags is None:
@@ -38,21 +49,43 @@ def add_flashcard(question, answer, tags=None):
         session.commit()
 
 
-def get_flashcards():
+@app.command()
+def get_flashcards(tag: Optional[str] = None):
     """Get all flashcards from the database."""
+
+    welcome()
+
     with Session(engine) as session:
-        cards = session.exec(select(Flashcard)).all()
+        if tag is None:
+            flashcards = session.exec(select(Flashcard)).all()
+        else:
+            flashcards = session.exec(
+                select(Flashcard)
+                .join(FlashcardTagLink)
+                .join(Tag)
+                .where(Tag.name == tag)
+            ).all()
 
-        for card in cards:
-            print(card)
-            print(card.tags)
+        for card in flashcards:
+            qa_console(card)
 
 
-def main():
-    create_db_and_tables()
-    add_flashcard("Tell me about yourself.", "I'm a software developer.", "Interview")
-    get_flashcards()
+def welcome():
+    """Welcome the user."""
+    print("Welcome to Flashy!")
+    print("Press Ctrl+C to exit.")
+    print("----------------------------------")
+    print("")
+
+
+def qa_console(card):
+    """Ask and answer a flashcard."""
+    print(f"[bold red]Question:[/bold red] [white]{card.question}[/white]")
+    time.sleep(2)
+    typer.prompt("Rate the difficulty of this question (1-5)")
+    print(f"[bold green]Answer:[/bold green] [white]{card.answer}[/white]")
+    print("----------------------------------")
 
 
 if __name__ == "__main__":
-    main()
+    app()
